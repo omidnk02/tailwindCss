692f11d61b506438ca2c23f6c5786116f60a4143
omid <omiidnk02@gmail.com>
2024-08-23 21:04:50 +0330

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+
+    extend: {
+      colors: {
+        primary: {
+          default: "#",
+          lighter: "#",
+          darker: "#",
+        },
+      },
+    },
+
     container: {
       center: true,
+      padding: "0.6rem",
     },
   },
+
   plugins: [],
 };
